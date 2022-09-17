6638d363d55912d1d8197feeb60f5b9445bf5160
vibovenkat123 <vibovenkat@gmail.com>
2022-09-17 17:33:24 -0500

    add average number function

--- src/analyze.ts
+++ src/analyze.ts
@@ -1,1 +1,12 @@
-class analyze {}
+class analyze {
+  average: number;
+  constructor(array: number[]) {
+    this.average = this.averageNumber(array);
+  }
+  averageNumber(array: number[], length: number = array.length - 1) {
+    if (length == 0) {
+      return array[length];
+    }
+    return array[length] + this.averageNumber(array, length - 1);
+  }
+}
