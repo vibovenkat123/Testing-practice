905ce36c070250f8bad15f50dd724e86f26385fc
vibovenkat123 <vibovenkat@gmail.com>
2022-09-17 11:42:27 -0500

    test subract function

--- test/calculator.test.ts
+++ test/calculator.test.ts
@@ -2,3 +2,6 @@ import { calculator } from "../src/calculator";
 it("add 2 + 3 to equal 5", () => {
   expect(calculator.add(2, 3)).toEqual(5);
 });
+it("subtract 6 - 4 to equal 2", () => {
+  expect(calculator.subtract(6, 4)).toEqual(2);
+});
