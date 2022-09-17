dccf544e5d98e4d472f325b07d60c83aaa8bd604
vibovenkat123 <vibovenkat@gmail.com>
2022-09-17 17:42:50 -0500

    tested average function

--- test/analyze.test.ts
+++ test/analyze.test.ts
@@ -3,3 +3,6 @@ let arrayAnalyze: analyze;
 beforeEach(() => {
   arrayAnalyze = new analyze([1, 4, 7, 9]);
 });
+it("can get average of the array", () => {
+  expect(arrayAnalyze.average).toEqual(5.25);
+});
