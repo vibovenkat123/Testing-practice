a1ce683d26373a3792d2addbae284ad91397bcc5
vibovenkat123 <vibovenkat@gmail.com>
2022-09-17 17:00:31 -0500

    test if string will be the same if shifted 26 places

--- test/caesar.test.ts
+++ test/caesar.test.ts
@@ -2,3 +2,7 @@ import caesar from "../src/caesar";
 it("Can shift 1 place", () => {
   expect(caesar("abc", 1)).toBe("bcd");
 });
+it("shifting 26 places is the same as original string", () => {
+  let string: String = "abc";
+  expect(caesar(string, 26)).toBe(string);
+});
