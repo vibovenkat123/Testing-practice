63e55726c60f6e0081c0de760c1c9ca592114147
vibovenkat123 <vibovenkat@gmail.com>
2022-09-17 11:43:33 -0500

    test multiply function

--- test/calculator.test.ts
+++ test/calculator.test.ts
@@ -5,3 +5,6 @@ it("add 2 + 3 to equal 5", () => {
 it("subtract 6 - 4 to equal 2", () => {
   expect(calculator.subtract(6, 4)).toEqual(2);
 });
+it("multiple 12 * 3 to equal 36", () => {
+  expect(calculator.multiply(12, 3)).toEqual(36);
+});
