18bd9e47a7a0a73b18d4a4d4ba46aff976c396bf
vibovenkat123 <vibovenkat@gmail.com>
2022-09-17 11:55:31 -0500

    test punctuation

--- test/calculator.test.ts
+++ test/calculator.test.ts
@@ -11,3 +11,15 @@ it("multiple 12 * 3 to equal 36", () => {
 it("divide 12 / 3 to equal 4", () => {
   expect(calculator.divide(12, 3)).toEqual(4);
 });
+it("adding punctuation", () => {
+  expect(calculator.add(1.2, 3.3)).toBeCloseTo(4.5);
+});
+it("subtracting punctuation", () => {
+  expect(calculator.subtract(3.3, 1.2)).toBeCloseTo(2.1);
+});
+it("multiplying punctuation", () => {
+  expect(calculator.multiply(1.2, 3.3)).toBeCloseTo(3.96);
+});
+it("dividing punctuation", () => {
+  expect(calculator.divide(28.38, 8.6)).toBeCloseTo(3.3);
+});
