f98e4954533326d9c38d24ade1f0cdee84786e9c
vibovenkat123 <vibovenkat@gmail.com>
2022-09-17 17:41:04 -0500

    made average function a sum function

--- src/analyze.ts
+++ src/analyze.ts
@@ -3,11 +3,15 @@ class analyze {
   constructor(array: number[]) {
     this.average = this.averageNumber(array);
   }
-  averageNumber(array: number[], length: number = array.length - 1) {
+  sum(array: number[], length: number = array.length - 1) {
     if (length == 0) {
       return array[length];
     }
-    return array[length] + this.averageNumber(array, length - 1);
+    return array[length] + this.sum(array, length - 1);
+  }
+  averageNumber(array: number[]) {
+    let sum: number = this.sum(array);
+    return sum / array.length;
   }
 }
 export default analyze;
--- test/analyze.test.ts
+++ test/analyze.test.ts
@@ -1,5 +1,5 @@
 import analyze from "../src/analyze";
-let arrayAnalyze;
+let arrayAnalyze: analyze;
 beforeEach(() => {
   arrayAnalyze = new analyze([1, 4, 7, 9]);
 });
