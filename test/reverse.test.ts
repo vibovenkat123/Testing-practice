1acb5c782ffa546de0b370148fd81ec109bd3fb6
vibovenkat123 <vibovenkat@gmail.com>
2022-09-17 11:36:08 -0500

    finish testing the reverse function

--- test/reverse.test.ts
+++ test/reverse.test.ts
@@ -3,3 +3,10 @@ import reverseString from "../src/reverse";
 it("reversing hello makes olleh", () => {
   expect(reverseString("hello")).toBe("olleh");
 });
+it("reverse will keep the same case", () => {
+  expect(reverseString("HELLO")).toBe("OLLEH");
+  expect(reverseString("tEsTinG")).toBe("GniTsEt");
+});
+it("works with spaces", () => {
+  expect(reverseString("testing 1 2 3")).toBe("3 2 1 gnitset");
+});
