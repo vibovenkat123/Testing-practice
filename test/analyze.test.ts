2b1b0ec5e27949626937de7ca56ca42f772e6ef6
vibovenkat123 <vibovenkat@gmail.com>
2022-09-17 17:45:05 -0500

    add todos for test

--- test/analyze.test.ts
+++ test/analyze.test.ts
@@ -6,3 +6,6 @@ beforeEach(() => {
 it("can get average of the array", () => {
   expect(arrayAnalyze.average).toEqual(5.25);
 });
+it.todo("can get minimum value");
+it.todo("can get maximum value");
+it.todo("can get length");
